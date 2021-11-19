58113a090ec7404153096a7a697c8ced67f0f07f
Yuval Koren <mygz.koren@gmail.com>
2021-11-19 12:41:01 -0800

    Support iOS 13 and MacOS 11 ahead of Xcode 13.2 release

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "AsyncExternalAccessory",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v12),
+        .iOS(.v13),
+        .macOS(.v11),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         //.package(name: "AsyncStream", path: "../AsyncStream")
-        .package(url: "https://github.com/AppAccount/AsyncStream", .upToNextMajor(from: "2.0.0"))
+        .package(url: "https://github.com/AppAccount/AsyncStream", .upToNextMajor(from: "2.1.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
